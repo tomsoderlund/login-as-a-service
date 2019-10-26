4f8b8fc44d916cd4177bc47d85ec249c7f6556f9
Tom Söderlund <tom@tomsoderlund.com>
2019-10-26 09:24:04 +0200

    Using pool.connect/release

--- api/people/[person].js
+++ api/people/[person].js
@@ -2,10 +2,20 @@ const { Pool } = require('pg')
 const { sql: { sqlFind } } = require('sql-wizard')
 
 const { config } = require('../../config/config')
-const pool = new Pool({ connectionString: config.databaseUrl })
 
 module.exports = async (req, res) => {
   // const { method, url, body, query } = req
-  const rows = await sqlFind(pool, 'person')
-  res.json(rows)
+  const pool = new Pool({ connectionString: config.databaseUrl })
+  pool.connect(async (err, client, release) => {
+    try {
+      if (err) throw new Error(err)
+      const rows = await sqlFind(client, 'person')
+      release()
+      res.json(rows)
+    }
+    catch (err) {
+      console.error(`Error: ${err.message || err}`)
+      res.json(err)
+    }
+  })
 }
