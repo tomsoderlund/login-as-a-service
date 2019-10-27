136ba855d5eb6da2010328a6ec564a8a39c8e9cf
Tom Söderlund <tom@tomsoderlund.com>
2019-10-27 22:05:08 +0100

    Lead API good

--- api/[app]/lead.js
+++ api/[app]/lead.js
@@ -14,12 +14,18 @@ module.exports = async (req, res) => {
     await handleRestAction(async () => {
       if (method === 'POST') {
         const { email, ...metadata } = body
-        const person = await sqlFindOrCreate(pool, 'person', { email }, { email, metadata }, { findRowByField: 'email', debug: true })
+        if (!email) throw new Error('Lead has no \'email\' property:400')
+        const person = await sqlFindOrCreate(pool, 'person', { email }, { email, metadata }, { findRowByField: 'email' })
         const app = await getAppByName(pool, appName)
         if (!app) throw new Error(`App '${appName}' not found:404`)
         const personAppIds = { person_id: person.id, app_id: app.id }
-        const personApp = await sqlFindOrCreate(pool, 'person_app', personAppIds, personAppIds, { debug: true })
-        res.json({ personApp })
+        const personAppRaw = await sqlFindOrCreate(pool, 'person_app', personAppIds, personAppIds, { findRowByField: 'person_id,app_id,token' })
+        // If newly-created, parse differently
+        // const personApp = personAppRaw.command === 'INSERT'
+        //   ? personAppRaw.rows[0]
+        //   : personAppRaw
+        // if (personAppRaw.command !== 'INSERT') throw new Error(`This person already exists in '${appName}' not found:409`)
+        res.json({ message: (personAppRaw.command === 'INSERT' ? 'New lead created' : 'Lead created') })
       } else {
         throw new Error(`${method} method not allowed:405`)
       }
