038bf4643e7b79fd1d28905486a053a5e60eb801
Tom Söderlund <tom@tomsoderlund.com>
2020-09-26 00:02:38 +0200

    /api/stats

---
+++ api/stats/index.js
@@ -0,0 +1,9 @@
+const { sql: { sqlFind } } = require('sql-wizard')
+
+const { handleRequest } = require('../../lib/handleRestAction')
+const { runDatabaseFunction } = require('../../lib/database')
+
+module.exports = async (req, res) => handleRequest(async (req, res) => runDatabaseFunction(async (pool) => {
+  const apps = await sqlFind(pool, 'app', { fields: ['name', 'count(person.id) as people'], join: ['person_app', 'person'], group: 'name', sort: 'people desc' })
+  res.json(apps)
+}), { req, res })
