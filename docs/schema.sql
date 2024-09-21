f4cf5a95b7bb53d99f83852d551a16e20fccbfb9
Tom Söderlund <tom@tomsoderlund.com>
2024-09-21 16:03:01 +0200

    SQL schema update

--- docs/schema.sql
+++ docs/schema.sql
@@ -6,7 +6,10 @@ CREATE TABLE app (
   slug character varying(64) UNIQUE,
   date_created timestamp with time zone DEFAULT now(),
   redirect_url character varying(128),
-  email_domain character varying(64)
+  email_domain character varying(64),
+  secret character varying(32) DEFAULT md5(random()::text) UNIQUE,
+  email_api_key character varying(50),
+  email_api_server character varying(50)
 );
 
 -- Table Definition ----------------------------------------------
@@ -26,5 +29,15 @@ CREATE TABLE person_app (
   person_id integer NOT NULL REFERENCES person(id) ON DELETE CASCADE,
   app_id integer NOT NULL REFERENCES app(id) ON DELETE CASCADE,
   secret character varying(32) DEFAULT md5(random()::text) UNIQUE,
-  metadata json
+  metadata json,
+  user_id character varying(32) DEFAULT md5(random()::text) UNIQUE,
+  username character varying(32),
+  can_login boolean DEFAULT true,
+  subscribe_email boolean DEFAULT true,
+  subscribe_sms boolean DEFAULT true,
+  date_created timestamp with time zone DEFAULT now(),
+  -- Stripe payments
+  purchase_session_id character varying(80),
+  subscription_session_id character varying(80),
+  purchase_credits integer
 );
